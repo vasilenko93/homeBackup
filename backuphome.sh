2d1c900757caeab04b1791e6f515ce89ead3592b
Alex vasilenko <vasilenko.alex93@gmail.com>
2015-05-31 22:16:32 -0700

    Restore single file

--- backuphome.sh
+++ backuphome.sh
@@ -4,8 +4,7 @@
 
 
 timeStamp=$(date +%Y%m%d_%H%M)
-userName=$(whoami)
-version=v1.5
+version=v1.7
 
 #Font Colors
 black=$(tput setaf 0)
@@ -47,21 +46,21 @@ function createNewBackup()
  cd ~
  #I give credit for this bit of the code to galaktos from Reddit
  #http://www.reddit.com/r/bash/comments/36378b/new_user_tar_ignore_folders/crabt79
- excludes=(.homeBack_Ups .cache .steam Downloads)
+ excludes=(.homeBack_Ups .cache .dropbox Dropox .steam .mozilla Downloads BTSync)
  excludesOptions() {
     for exclude in "${excludes[@]}"; do
         echo "--exclude=$exclude"
     done
  }
- tar -cvzf ~/.homeBack_Ups/$timeStamp.tar.gz $(excludesOptions) .
  #end of credit
+ tar -cvzf ~/.homeBack_Ups/$timeStamp.tar.gz $(excludesOptions) . > ~/.homeBack_Ups/$timeStamp.files
 
  #makes the backup only accessible by owner, and not writable
  chmod 500 ~/.homeBack_Ups/$timeStamp.tar.gz
 
  echo ""
  echo "================================================================="
- echo -e "${green}${userName} a backup of your home folder was created."
+ echo -e "${green}${USER} a backup of your home folder was created."
  echo -e "${blue}${timeStamp}${green}.tar.gz"
  read -p "Press [Enter] to return back to main menu."
 
@@ -80,7 +79,7 @@ function restoreEntireBackup()
 |===============================================================|
 "
  if [ ${fileArray[0]} == "*.tar.gz" ]; then
-	numberOfBackups=0
+ 	numberOfBackups=0
  fi
  if [ $numberOfBackups -ge 1 ]; then
  	i=0
@@ -91,19 +90,19 @@ function restoreEntireBackup()
 	else
     	echo -e "${cyan}$numberOfBackups ${green}backups found in: ${blue}$(pwd)"
 	fi
- 	echo "${green}------------------------------------------"
-  echo -e "| ${red}#${green} | Year Month Day Hour Minute (24 hour)"
-  echo "|---|-------------------------------------"
+    echo "${green}------------------------------------------"
+    echo -e "| ${red}#${green} | Year Month Day Hour Minute (24 hour)"
+    echo "|---|-------------------------------------"
 
-  for (( i; i < $numberOfBackups; i++ ))
-  do
-  	num=$(($i + 1))
-	  echo -e "| ${red}${num} ${green}| ${fileArray[i]}"
-  done
+    for (( i; i < $numberOfBackups; i++ ))
+    do
+        num=$(($i + 1))
+        echo -e "| ${red}${num} ${green}| ${fileArray[i]}"
+    done
 	echo ""
 	echo "Choose a backup to restore"
-  echo -ne "or anything else to cancel ${red}#${green} "
- 	goodInput=0
+    echo -ne "or anything else to cancel ${red}#${green} "
+    goodInput=0
 	read input
 
 	if [ $input -ge "1" -a $input -le $numberOfBackups ]; then
@@ -128,23 +127,92 @@ function restoreEntireBackup()
 
 function restoreFile()
 {
-    echo -e "${red}TODO"
-    read -p "${green}Press [Enter] to return to menu."
+    cd ~/.homeBack_Ups
+    backupArray=(*.tar.gz)
+    filesArray=(*.files)
+    numberOfBackups=${#backupArray[@]}
 
+    clear
+    echo -e "${green}
+|===============================================================|
+|======================= ${red}Restore a file ${green}========================|
+|===============================================================|
+"
+    if [ ${backupArray[0]} == "*.tar.gz" ]; then
+        numberOfBackups=0
+    fi
+    if [ $numberOfBackups -ge 1 ]; then
+        filename=""
+        while [ ${#filename} -lt 1 ]
+        do
+            echo -n "${green}Type the file you want to restore ${red}> "
+            read filename
+        done
+
+        filesfound=()
+        index=0
+        selection=1
+        for backup in "${backupArray[@]}"
+        do
+            echo -e "${red}${backup} |========================>${green}"
+            filesfound_temp=( $(cat ${backup%%.*}.files | grep -i ${filename}) )
+            # Problem, does not remove duplicates
+            filesfound=("${filesfound[@]}" "${filesfound_temp[@]}")
+            positions[index]=0
+            for file in "${filesfound_temp[@]}"
+            do
+                echo -e "| ${red}${selection} ${green}|  ${file}"
+                positions[index]=$selection
+                ((selection++))
+            done
+            ((index++))
+        done
+
+        echo ""
+        echo "Choose a file to restore"
+	    echo -ne "or 'q' to cancel ${red}# ${green}"
+        goodInput=0
+        read input
+
+        if [[ "${input[0]}" == "q" || "${input[0]}" == "Q" ]]; then
+            goodInput=0
+        elif [ $input -ge 1 -a $input -le $selection ]; then
+            goodInput=1
+        fi
+
+        if [ $goodInput -eq 1 ]; then
+            restore_index=0
+            while [ ${positions[restore_index]} -lt $input  ]
+            do
+                ((restore_index++))
+            done
+            filesindex=$((input-1))
+            tar -xf ${backupArray[restore_index]} -C ~ ${filesfound[filesindex]}
+            echo -e "${green}File ${blue}${filesfound[filesindex]} ${green}restored."
+            read -p "${green}Press [Enter] to return back to main menu."
+        else
+            read -p "${green}Press [Enter] to return back to main menu."
+        fi
+    else
+        echo -e "${green}No backups found in: ${blue}$(pwd)"
+        read -p "Press [Enter] to return to menu."
+    fi
 }
 
 function deleteBackup()
 {
     cd ~/.homeBack_Ups
-    fileArray=(*.tar.gz)
-    numberOfBackups=${#fileArray[@]}
+    backupArray=(*.tar.gz)
+    filesArray=(*.files)
+    numberOfBackups=${#backupArray[@]}
+
     clear
     echo -e "${green}
 |===============================================================|
 |======================= ${red}Delete a backup ${green}=======================|
 |===============================================================|
 "
-    if [ ${fileArray[0]} == "*.tar.gz" ]; then
+    if [ ${backupArray[0]} == "*.tar.gz" ]; then
         numberOfBackups=0
     fi
     if [ $numberOfBackups -ge 1 ]; then
@@ -152,36 +220,39 @@ function deleteBackup()
         #one=1
         backUpPicked=0
 		if [ $numberOfBackups -eq 1 ]; then
-			 echo -e "${cyan}$numberOfBackups ${gree}backup found in: ${blue}$(pwd)"
+			 echo -e "${cyan}$numberOfBackups ${green}backup found in: ${blue}$(pwd)"
 		else
-			echo -e "${cyan}$numberOfBackups ${gree}backups found in: ${blue}$(pwd)"
+			echo -e "${cyan}$numberOfBackups ${green}backups found in: ${blue}$(pwd)"
         fi
-        echo "${gree}------------------------------------------"
+        echo "${green}------------------------------------------"
         echo -e "| ${red}# ${green}| Year Month Day Hour Minute (24 hour)"
         echo "|---|-------------------------------------"
 
         for (( i; i < $numberOfBackups; i++ ))
         do
             num=$(($i + 1))
-            echo -e "| ${red}${num} ${green}|  ${fileArray[i]}"
+            echo -e "| ${red}${num} ${green}|  ${backupArray[i]}"
         done
         echo ""
         echo "Choose a backup to delete"
-	    echo -ne "or anything else to cancel ${red}# ${green}"
+	    echo -ne "or 'q' to cancel ${red}# ${green}"
         goodInput=0
         read input
 
-        if [ $input -ge "1" -a $input -le $numberOfBackups ]; then
+        if [[ "${input[0]}" == "q" || "${input[0]}" == "Q" ]]; then
+            goodInput=0
+        elif [ $input -ge "1" -a $input -le $numberOfBackups ]; then
             goodInput=1
         fi
 
-        if [ $goodInput -eq "1" ]; then
+        if [ $goodInput -eq 1 ]; then
             clear
             cd ~/.homeBack_Ups
             backUpPicked=$(($input - 1))
-            echo -en "${green}Deleting ${red}${fileArray[backUpPicked]} ${green}..."
-		    chmod 600 ${fileArray[backUpPicked]}
-            rm ${fileArray[backUpPicked]}
+            echo -en "${green}Deleting ${red}${backupArray[backUpPicked]} ${green}..."
+		    chmod 600 ${backupArray[backUpPicked]}
+            rm ${backupArray[backUpPicked]}
+            rm ${filesArray[backUpPicked]}
             echo -e "${red} Deleted."
             read -p "${green}Press [Enter] to return back to main menu."
         else
@@ -208,7 +279,7 @@ do
  ${colreset}"
  cd ~
  echo -e "${green}Version:\t\t ${blue}${version}"
- echo -e "${green}Active user:\t\t ${blue}${userName}"
+ echo -e "${green}Active user:\t\t ${blue}${USER}"
  echo -e "${green}User home directory:\t ${blue}$(pwd)"
  echo ""
  echo -e "${green}1) Backup home directory."
@@ -236,7 +307,7 @@ do
 	clear
   ;;
   *)
-	echo -p "${red}Error: Invalid input."
+	echo -e "${red}Error: Invalid input."
 	read -p "${green}Press [Enter] to try again."
   ;;
  esac
